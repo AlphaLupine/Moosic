c48fcb6d3e08674b3a7599d35b1e7591a8a2801a
AlphaLupine <matthewduff0912@gmail.com>
2020-12-26 02:19:45 +0000

    ✨ Added ability where the trackStart event will attempt to edit the previous now playing message if one exists and is the latest message before sending a new one

--- src/listeners/MusicManager/TrackStartListener.ts
+++ src/listeners/MusicManager/TrackStartListener.ts
@@ -1,7 +1,8 @@
 import { Listener } from 'discord-akairo';
-import { TextChannel } from 'discord.js';
+import type { TextChannel } from 'discord.js';
 import { Player } from 'erela.js';
 import  Utils  from '../../lib/util/Util';
+import type { Track } from "erela.js/structures/Player";
 
 export default class TrackStartListener extends Listener {
     constructor() {
@@ -11,10 +12,12 @@ export default class TrackStartListener extends Listener {
         });
     }
 
-    exec(player: Player, track) {
-       const channel = this.client.channels.cache.get(player.textChannel!);
-        (channel as TextChannel).send(new this.client.embed().setMain()
-            .setTitle('Now Playing:')
+    async exec(player: Player, track: Track) {
+        const guild = this.client.guilds.cache.get(player.guild);
+        const channel = guild!.channels.cache.get(player.textChannel!) as TextChannel;
+
+        const embed = new this.client.embed().setMain()
+            .setTitle("Now Playing:")
             .setDescription(
                 [
                     `Song Name: [${track.title}](${track.uri})`,
@@ -22,7 +25,17 @@ export default class TrackStartListener extends Listener {
                     `Duration: ${Utils.msConversion(track.duration)?.string}`
                 ].join('\n')
             )
-            .setThumbnail(track.thumbnail)
-        );
+            .setThumbnail(track.thumbnail!);
+
+        if (channel) {
+            const nowPlayingMessageID = this.client.NowPlayingCache.get(channel.id);
+            if (nowPlayingMessageID && channel.lastMessageID === nowPlayingMessageID) {
+                const lastMessage = channel.messages.cache.get(nowPlayingMessageID);
+                if (lastMessage) return lastMessage.edit(embed);
+            }
+
+            const nowPlayingMessage = await channel.send(embed);
+            return this.client.NowPlayingCache.set(channel.id, nowPlayingMessage.id);
+        }
     }
 }
